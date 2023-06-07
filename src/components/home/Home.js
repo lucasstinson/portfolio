ddd7d39f46fce491d6a1ab797b729b329282586c
Luke <user@pop-os.localdomain>
2023-06-07 16:13:16 -0500

    created the initial screen for the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import userLogo from "../../assets/images/characterLogo.png";
+import "./home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return "Home";
+  return (
+    <div className="Home">
+      <div className="intro">
+        <h2 className="name-intro">Hi! I'm Luke.</h2>
+        <h1 className="info-intro">
+          I'm an aspiring web developer based in Nashville.
+        </h1>
+        <div className="learn-more-container">
+          <Link to="/about">
+            <button className="learn-more">Get to Know Me</button>
+          </Link>
+        </div>
+      </div>
+      <div className="intro-image">
+        <img src={userLogo} className="user-logo"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
+
+// Hi! I'm Luke, an aspiring web developer based in Nashville.
+// Learn More - button
