570530a07d6b0ec34aaec53156d7491cb4c5a8e9
Luke <user@pop-os.localdomain>
2023-06-07 16:38:11 -0500

    update styling for mobile

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,18 +7,22 @@ const Home = () => {
   return (
     <div className="Home">
       <div className="intro">
-        <h2 className="name-intro">Hi! I'm Luke.</h2>
-        <h1 className="info-intro">
-          I'm an aspiring web developer based in Nashville.
-        </h1>
-        <div className="learn-more-container">
-          <Link to="/about">
-            <button className="learn-more">Get to Know Me</button>
-          </Link>
+        <div className="intro-text">
+          <h2 className="name-intro">
+            Hi! I'm <span className="name">Luke</span>.
+          </h2>
+          <h1 className="info-intro">
+            I'm an aspiring web developer based in Nashville.
+          </h1>
+          <div className="learn-more-container">
+            <Link to="/about">
+              <button className="learn-more">Get to Know Me</button>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="intro-image">
-        <img src={userLogo} className="user-logo"></img>
+        <div className="intro-image">
+          <img src={userLogo} className="user-logo"></img>
+        </div>
       </div>
     </div>
   );
