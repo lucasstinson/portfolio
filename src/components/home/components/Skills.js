26b07c2c493539345886a30f205c0a03ee34ddce
Luke <lucasstinson@gmail.com>
2023-06-15 15:20:46 -0500

    add additional skills and update mobile styling

--- src/components/home/components/Skills.js
+++ src/components/home/components/Skills.js
@@ -5,11 +5,14 @@ import git from "../../../assets/images/tech-icons/git.png";
 import html from "../../../assets/images/tech-icons/html.png";
 import javascript from "../../../assets/images/tech-icons/javascript.png";
 import jest from "../../../assets/images/tech-icons/jest.png";
+import node from "../../../assets/images/tech-icons/nodejs.png";
 // import linux from "../../../assets/images/tech-icons/linux.png";
 import npm from "../../../assets/images/tech-icons/npm.png";
+import postgres from "../../../assets/images/tech-icons/postgres.png";
 import python from "../../../assets/images/tech-icons/python.png";
 import reactrouter from "../../../assets/images/tech-icons/react-router.svg";
 import react from "../../../assets/images/tech-icons/react.png";
+import typescript from "../../../assets/images/tech-icons/typescript.png";
 import webpack from "../../../assets/images/tech-icons/webpack.png";
 
 const Skills = () => {
@@ -20,10 +23,13 @@ const Skills = () => {
     { class: "html", image: html, title: "HTML" },
     { class: "javascript", image: javascript, title: "Javascript" },
     { class: "jest", image: jest, title: "Jest" },
+    { class: "node", image: node, title: "Node" },
+    { class: "postgres", image: postgres, title: "Postgres" },
     { class: "npm", image: npm, title: "NPM" },
     { class: "python", image: python, title: "Python" },
     { class: "react-router", image: reactrouter, title: "React Router" },
     { class: "react", image: react, title: "React" },
+    { class: "typescript", image: typescript, title: "Typescript" },
     { class: "webpack", image: webpack, title: "Webpack" },
   ];
 
